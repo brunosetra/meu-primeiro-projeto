f348608ccff153b285b5f02dc4f954967f09ec26
bruno.setra <bruno.setra@detes-030231.casadamoeda.gov.br>
2019-01-09 14:54:30 -0200

    utilizacao do arquivo application.properties

--- src/main/java/com/kazela/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/kazela/api/MeuPrimeiroProjetoApplication.java
@@ -1,7 +1,10 @@
 package com.kazela.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
@@ -11,5 +14,16 @@ public class MeuPrimeiroProjetoApplication {
 		System.out.println("Meu primeiro Projeto...");
 	}
 
+
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
+
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.format("### Quantidade  de elementos por pagina = %d%n", this.qtdPorPagina);
+		};
+	}
 }
 
